26f8266267f9b3298ac0fad7626ac2992140a150
xxx <xxx@web.de>
2022-11-14 19:51:46 +0100

    master-c02-Class1.get02() added

--- src/pk1/Class1.java
+++ src/pk1/Class1.java
@@ -2,8 +2,11 @@ package pk1;
 
 public class Class1 {
     public String get01(){
-        return this.getClass().getCanonicalName() + ".get01()-c01-";
+        return this.getClass().getCanonicalName() + ".get01()-c01-master";
         //return this.getClass().getProtectionDomain().getCodeSource().getLocation().toString();
+    }
+    public String get02() {
+        return this.getClass().getCanonicalName() + ".get02()-c02-master";
     }
     public static void main(String[] args) {
         Class1 obj = new Class1();
